87429dbc7a7b9fe43ce6f992a532e3c31c5c116b
joel-mainey <grunty8848@gmail.com>
2022-03-21 09:26:31 +1300

    Initial commit of this app

---
+++ calculate.js
@@ -0,0 +1,10 @@
+function calculate()
+{
+    let carEconomy = document.getElementById("carEconomy").value;
+    let distance = document.getElementById("distance").value;
+    let fuelCost = document.getElementById("fuelCost").value;
+
+    let litreCost = (carEconomy * fuelCost) / 100;
+
+    document.getElementById("display").innerHTML = "This trip cost you $" + Math.round(litreCost * distance);
+}
